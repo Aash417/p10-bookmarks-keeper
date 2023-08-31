e8206b73a6ccfafc511166d5386e0e0a07de98b2
aashish_k <aashishkathat00@gmail.com>
2023-08-31 22:12:26 +0530

    form validation

--- script.js
+++ script.js
@@ -12,13 +12,50 @@ function showModal() {
   websiteNameEl.focus();
 }
 
+function storeBookmark(e) {
+  e.preventDefault();
+
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+
+  if (!urlValue.includes('https://', 'https://')) {
+    urlValue = `https://${urlValue}`;
+  }
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+}
+
+// Validate form
+function validate(nameValue, urlValue) {
+  const expression =
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+
+  // gaurd clause
+  if (!nameValue || !urlValue)
+    alert(`Please submit both valued in input fields`);
+
+  const regex = new RegExp(expression);
+  if (urlValue.match(regex)) {
+    console.log('mat');
+  }
+  if (!urlValue.match(regex)) {
+    console.log('didnt');
+  }
+
+  return true;
+}
+
+// Event listner
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => {
   modal.classList.remove('show-modal');
 });
-
 window.addEventListener('click', (e) => {
   //   e.target.classList[0] === 'modal-container'
   e.target === modal ? modal.classList.remove('show-modal') : false;
   //   console.log(e.target.classList[0]);
+  e.p;
 });
+
+bookmarkForm.addEventListener('submit', storeBookmark);
